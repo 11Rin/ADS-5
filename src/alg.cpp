fabb69113d4bd19868a9a2e5c310f6c2a36bb5e9
Irina <116427830+11Rin@users.noreply.github.com>
2023-04-10 12:45:16 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -32,10 +32,9 @@ std::string infx2pstfx(std::string inf) {
       postf += symbol;
       postf += ' ';
     } else {
-        if (stack.get() < prior || prior == 0 || stack.isEmpty())
-          stack.push(symbol);
-        else if (symbol == ')') 
-        {
+        if (stack.get() < prior || prior == 0 || stack.isEmpty()) {
+          stack.push(symbol);}
+        else if (symbol == ')') {
           char sm = stack.get();
           while (getPrior(sm) >= prior) {
             postf += sm;
